cf66b72de3ac544cdc333a4f1745c318d32d1c87
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-01-29 22:41:44 +0600

    78-2 done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 
@@ -11,6 +11,11 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const poppins = Poppins({
+  weight: ['400', '600', '700'],
+  subsets: ['latin'],
+})
 
 export const metadata = {
   title: {
@@ -25,11 +30,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.className} antialiased`}
       >
         <Navbar/>
         
-        <main className="min-h-[600px]">
+        <main className="min-h-[600px] w-11/12 mx-auto my-8">
         {children}
         </main>
 
--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,5 +1,12 @@
 import Link from "next/link";
 import MealSearchInput from "./components/MealSearchInput";
+import Image from "next/image";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+	weight: ['400'],
+	subsets: ['latin'],
+})
 
 export const metadata = {
 	title: "All Meals",
@@ -35,11 +42,12 @@ export default async  function MealsPage({searchParams}) {
 	<div className='flex justify-center'>
 		<MealSearchInput/>
 	</div>
-	<div className='grid grid-cols-4 md:grid-cols-2 lg:grid-cols-4 sm:grid-cols-1 gap-4'>
+	<div className='grid grid-cols-3 md:grid-cols-2 lg:grid-cols-3 sm:grid-cols-1 gap-4'>
 		{/* <p>{JSON.stringify(meals)}</p> */}
 		{meals?.map((singleMeal) =>{
 			return (
-				<div className='border-4 border-slate-600'>
+				<div className={`${roboto.className} border-4 border-slate-600 p-2`} key={singleMeal?.idMeal} >
+					<Image width={641} height={641} src={singleMeal?.strMealThumb} alt={singleMeal?.strMeal} />
 					<p className='text-2xl font-bold'>{singleMeal?.strMeal}</p>
 					<p>{singleMeal?.strInstructions}</p>
 					<Link className="bg-rose-500 p-2 rounded-lg" href={`/meals/${singleMeal.idMeal}`}>Details</Link>
